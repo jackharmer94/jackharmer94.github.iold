b314f40c22d5c9dd5cdb3356cab03ee9c6968940
jackharmer94 <85137867+jackharmer94@users.noreply.github.com>
2024-02-17 00:13:07 +0000

    Delete BSM App directory

--- BSM App/backend/config/dbConfig.js.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    host: 'localhost',
-    port: '3306',
-    user: 'your_database_user',
-    password: 'your_database_password',
-    database: 'your_database_name'
-  }
-};
